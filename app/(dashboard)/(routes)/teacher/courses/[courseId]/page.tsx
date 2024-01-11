438b1bcf8441f97f38ab09f8b901080e5dfb4ebf
leeeyoo <leeeyoo@gmail.com>
2024-01-11 17:41:07 +0900

    feat: category

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -8,6 +8,7 @@ import { IconBadge } from "@/components/icon-badge"
 import { TitleForm } from "./_components/title-form"
 import { DescriptionForm } from "./_components/description-form"
 import { ImageForm } from "./_components/image-form"
+import { CategoryForm } from "./_components/category-form"
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth()
@@ -16,6 +17,10 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 
   const course = await db.course.findUnique({
     where: { id: params.courseId }
+  })
+
+  const categories = await db.category.findMany({
+    orderBy: { name: "asc" }
   })
 
   if (!course) { return redirect("/") }
@@ -64,6 +69,14 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
           <ImageForm
             initialData={course}
             courseId={course.id}
+          />
+          <CategoryForm
+            initialData={course}
+            courseId={course.id}
+            options={categories.map((category) => ({
+              label: category.name,
+              value: category.id,
+            }))}
           />
         </div>
       </div>
