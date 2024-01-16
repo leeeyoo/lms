b88a6df924a77f903a2c33335ee468b1f2702adf
leeeyoo <leeeyoo@gmail.com>
2024-01-16 18:58:05 +0900

    feat: attachment

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,6 +1,6 @@
 import { auth } from "@clerk/nextjs"
 import { redirect } from "next/navigation"
-import { CircleDollarSign, LayoutDashboard, ListChecks } from "lucide-react"
+import { CircleDollarSign, File, LayoutDashboard, ListChecks } from "lucide-react"
 
 import { db } from "@/lib/db"
 import { IconBadge } from "@/components/icon-badge"
@@ -9,6 +9,8 @@ import { TitleForm } from "./_components/title-form"
 import { DescriptionForm } from "./_components/description-form"
 import { ImageForm } from "./_components/image-form"
 import { CategoryForm } from "./_components/category-form"
+import { PriceForm } from "./_components/price-form"
+import { AttachmentForm } from "./_components/attachment-form"
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth()
@@ -16,7 +18,16 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   if (!userId) { return redirect("/") }
 
   const course = await db.course.findUnique({
-    where: { id: params.courseId }
+    where: {
+      id: params.courseId
+    },
+    include: {
+      attachments: {
+        orderBy: {
+          createdAt: "desc"
+        }
+      }
+    }
   })
 
   const categories = await db.category.findMany({
@@ -98,6 +109,22 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
                 Sell your course
               </h2>
             </div>
+            <PriceForm
+              initialData={course}
+              courseId={course.id}
+            />
+          </div>
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={File} />
+              <h2 className="text-xl">
+                Resources & Attachments
+              </h2>
+            </div>
+            <AttachmentForm
+              initialData={course}
+              courseId={course.id}
+            />
           </div>
         </div>
       </div>
